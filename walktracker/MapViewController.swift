017d11138b8f4e0c05c7a283f7c58fd8d8ebaf42
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2015-01-09 12:30:36 +0100

    Initial commit

---
+++ walktracker/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  walktracker
+//
+//  Created by Kevin VanderLugt on 1/9/15.
+//  Copyright (c) 2015 Alpine Pipeline. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
